f76cb58f27ded675b23733a6e9a8ca7067758daf
DenizeScheffold <denizescheffold@gmail.com>
2023-02-20 20:15:47 +0100

    add set week

--- src/components/SetDayComponent.jsx
+++ src/components/SetDayComponent.jsx
@@ -10,6 +10,12 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import {Button } from "@mui/material";
 import WeekComponent from "./WeekComponent"
 //import { useParams } from "react-router-dom";
 
@@ -17,13 +23,31 @@ function SetDay() {
   const [dayInfo, setDayInfo] = React.useState([]
   );
   
-
-  // let {userId} = useParams();
-
-  React.useEffect(() => {
-    loadSetDay();
+  const [values, setValues] = React.useState({
+    weekNumber: ""
   });
 
+  
+  const handleChange = (prop) => (e) => {
+    setValues({ ...values, [prop]: e.target.value });
+    console.log(values.data);
+  };
+  // let {userId} = useParams();
+
+ 
+  const handleSubmit = async () => {
+      const result = await axios.get(
+        `http://localhost:8080/api/getDaysNotSet/${values.weekNumber}/1`,
+        //${userId}`,
+    { 
+  },{})
+  console.log(result);
+    console.log(result.data[0].userId,);
+    setDayInfo(result.data);
+};
+/* React.useEffect(() => {
+    loadSetDay();
+  });
   const loadSetDay = async () => {
     const result = await axios.get(
       `http://localhost:8080/api/getDaysNotSet/1`,
@@ -34,10 +58,51 @@ function SetDay() {
     console.log(result);
     console.log(result.data[0].userId,);
     setDayInfo(result.data);
-  };
+  }; 
+  */
 
   return (
     <div className="Day">
+      <form onSubmit={handleSubmit}>
+    <Grid
+      container
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
+      maxWidth="90%"
+      marginTop="auto"
+      marginLeft="4%"
+      spacing={2}
+    >
+      <Grid item>
+        <Typography variant="h2">Set Plan</Typography>
+      </Grid>
+
+      <Grid item sx={{ width: 0.5 }}>
+        <FormControl fullWidth>
+          <InputLabel >
+            Week Number
+          </InputLabel>
+          <OutlinedInput
+            label="weekNumber"
+            value={values.weekNumber}
+           onChange={handleChange("weekNumber")}
+            sx={{ borderRadius: "29px" }}
+          />
+        </FormControl>
+        </Grid>
+      </Grid>
+      
+      <Button
+            fullWidth
+            variant="contained"
+            type="submit"
+            >
+           Select week
+            </Button>
+    </form>
+
+
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
