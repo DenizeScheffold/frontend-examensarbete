38eee6e853b772e555cf226397dd44f0702f76c3
dscheffold <denizescheffold@gmail.com>
2023-03-02 09:01:05 +0100

    refactor view week

--- src/ViewWeek.js
+++ src/ViewWeek.js
@@ -11,16 +11,29 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 //import { useParams } from "react-router-dom";
 
 function ViewWeek() {
   const [dayInfo, setDayInfo] = React.useState([]
   );
+  const [values, setValues] = React.useState({
+    weekNumber: ""
+  });
 
   React.useEffect(() => {
  //   calculateDays();
     loadProcessedDays();
   });
+
+  const handleChange = (prop) => (e) => {
+    setValues({ ...values, [prop]: e.target.value });
+    console.log(values.data);
+  };
 /* 
   const calculateDays = async () => {
     const result = await axios.get(
@@ -45,13 +58,41 @@ function ViewWeek() {
 
   return (
     <div className="Day">
+            <form onSubmit={handleChange}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="space-between"
+          alignItems="center"
+          maxWidth="90%"
+          marginTop="auto"
+          marginLeft="4%"
+          spacing={2}
+        >
+          <Grid item>
+            <Typography variant="h2">Choose week</Typography>
+          </Grid>
+
+          <Grid item sx={{ width: 0.5 }}>
+            <FormControl fullWidth>
+              <InputLabel >
+                Week Number
+              </InputLabel>
+              <OutlinedInput
+                label="weekNumber"
+                value={values.weekNumber}
+                onChange={handleChange("weekNumber")}
+                sx={{ borderRadius: "29px" }}
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+      </form>
+
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Your Profile</TableCell>
-              <TableCell align="right">day id</TableCell>
-              <TableCell align="right">week number</TableCell>
               <TableCell align="right">date</TableCell>
               <TableCell align="right">activity</TableCell>
               <TableCell align="right">possible </TableCell>
@@ -62,10 +103,7 @@ function ViewWeek() {
           {dayInfo.map((day) => (
               <TableRow>
                 <TableCell component="th" scope="row">
-                  {day.userId}
                 </TableCell>
-                <TableCell align="right">{day.dayId}</TableCell>
-                <TableCell align="right">{day.weekNumber}</TableCell>
                 <TableCell align="right">{day.dayDate}</TableCell>
                 <TableCell align="right">{day.activity}</TableCell>
                 <TableCell align="right">{day.possible.toString()}</TableCell>
