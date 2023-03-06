91794d6a219b95bf54152401663cdd93ec49f70c
dscheffold <denizescheffold@gmail.com>
2023-03-06 13:54:42 +0100

    Updates

---
+++ precache-manifest.2870d02d398c26e3912150f27bb31f04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5bd1f5e327721fe80bf",
+    "url": "/static/css/main.6f212f51.chunk.css"
+  },
+  {
+    "revision": "a5bd1f5e327721fe80bf",
+    "url": "/static/js/main.a5bd1f5e.chunk.js"
+  },
+  {
+    "revision": "2b4e57923141e0aa228a",
+    "url": "/static/js/1.2b4e5792.chunk.js"
+  },
+  {
+    "revision": "8a07bb8d3648bf8a3cb8",
+    "url": "/static/css/2.f6f95b11.chunk.css"
+  },
+  {
+    "revision": "8a07bb8d3648bf8a3cb8",
+    "url": "/static/js/2.8a07bb8d.chunk.js"
+  },
+  {
+    "revision": "7f8b219cda3dac616ce9",
+    "url": "/static/js/runtime~main.7f8b219c.js"
+  },
+  {
+    "revision": "45665fad242d209e7a8cb8df5669ab41",
+    "url": "/index.html"
+  }
+];
